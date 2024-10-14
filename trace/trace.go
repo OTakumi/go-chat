bfd2c70b6154c77f6363dee2f556288447ebf1e6
OTakumi <takumi.o.0410@gmail.com>
2024-10-14 15:16:01 +0900

    modified: trace.go, new file: trace_test.go

--- trace/trace.go
+++ trace/trace.go
@@ -1,6 +1,24 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // Trace is the interface that describes the behavior of a trace.
 type Tracer interface {
 	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
 }
